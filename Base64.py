f46144089d226d59cfae44677310d3b87edff4be
Dgaming10 <danny.umansky@gmail.com>
2024-01-08 23:39:20 +0200

    make the base structure for base64 encryption

---
+++ Base64.py
@@ -0,0 +1,12 @@
+
+class Base64:
+    base64_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+
+
+    @staticmethod
+    def Encrypt(str_to_encrypt) -> str:
+        pass
+
+    @staticmethod
+    def Decrypt(str_to_decrypt) -> str:
+        pass
