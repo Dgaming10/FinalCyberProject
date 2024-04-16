2122548410f5bd7d54a0557fac06e03d563b351b
Dgaming10 <85754917+Dgaming10@users.noreply.github.com>
2024-04-16 17:42:52 +0300

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,9 +1,1 @@
-import pymongo
-from bson import ObjectId
-from gridfs import GridFS
-
-connection_string = ("mongodb+srv://root:qazpoi12345@cyberprojectdb.5cnsgy6.mongodb.net/?retryWrites"
-                                   "=true&w=majority&ssl=true")
-client = pymongo.MongoClient(connection_string)
-db = client["CyberProjectDB"]
-print(db["mails"].find_one({'_id': ObjectId('65da2ff97be6e9f10a41b0a8')}, {}).get("recipients")[0]["email"])
+#FILE FOR TESTING
