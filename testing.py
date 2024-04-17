81429cbbdb556346312ad17ff2f9e485afd90b26
Dgaming10 <danny.umansky@gmail.com>
2024-04-17 22:29:28 +0300

    changed testing.py

--- testing.py
+++ testing.py
@@ -6,5 +6,5 @@ from Base64 import Base64
 
 client = pymongo.MongoClient(globals_module.CONNECTION_STRING)
 db = client["CyberProjectDB"]
-print(db["mails"].find_one({'_id': ObjectId('661e8e09afe3fb88fec9efa6')}, {}))
-print(Base64.Encrypt('itay.lisker@mun.com'))
+print(db["mails"].find_one({'_id': ObjectId('id')}, {}))
+print(Base64.Encrypt('name@mun.com'))
