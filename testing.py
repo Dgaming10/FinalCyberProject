12d5eca376ff33a8a17b42f1f92ac00607adc680
Dgaming10 <danny.umansky@gmail.com>
2024-04-17 22:29:12 +0300

    changed testing.py

--- testing.py
+++ testing.py
@@ -1,1 +1,10 @@
-#FILE FOR TESTING
+import pymongo
+from bson import ObjectId
+
+import globals_module
+from Base64 import Base64
+
+client = pymongo.MongoClient(globals_module.CONNECTION_STRING)
+db = client["CyberProjectDB"]
+print(db["mails"].find_one({'_id': ObjectId('661e8e09afe3fb88fec9efa6')}, {}))
+print(Base64.Encrypt('itay.lisker@mun.com'))
